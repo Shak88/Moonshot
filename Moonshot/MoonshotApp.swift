75d141bb716b721da8576214eb77695eb06da520
Shokri <shokre4@hotmail.com>
2022-05-30 21:42:08 +0400

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Shokri Alnajjar on 30/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
