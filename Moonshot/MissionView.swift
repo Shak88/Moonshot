62c8f77f98bbc8491cd6e0ecc9fec898d4549dff
Shokri Alnajjar <shokre4@hotmail.com>
2022-06-04 14:28:18 +0400

    adding extra view

--- Moonshot/MissionView.swift
+++ Moonshot/MissionView.swift
@@ -53,35 +53,8 @@ struct MissionView: View {
                             .padding(.bottom,5)
                     }
                     .padding(.horizontal)
-                    
-                    ScrollView(.horizontal, showsIndicators: false) {
-                        HStack{
-                            ForEach(crew, id:\.role) { crewMember in
-                                NavigationLink {
-                                    AstronautView(astronaut: crewMember.astronaut)
-                                } label: {
-                                    HStack {
-                                        Image(crewMember.astronaut.id)
-                                            .resizable()
-                                            .frame(width:104, height:72)
-                                            .clipShape(Capsule())
-                                            .overlay(
-                                                Capsule()
-                                                    .strokeBorder(.white, lineWidth: 1)
-                                            )
-                                        VStack(alignment:.leading) {
-                                            Text(crewMember.astronaut.name)
-                                                .foregroundColor(.white)
-                                                .font(.headline)
-                                            Text(crewMember.role)
-                                                .foregroundColor(.secondary)
-                                        }
-                                    }
-                                    .padding(.horizontal)
-                                }
-                            }
-                        }
-                    }
+                    //Scrolview
+                    CrewMemberView(crewMember: crew)
                 }
                 .padding(.bottom)
                 
